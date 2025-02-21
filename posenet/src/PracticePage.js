43dfbf0eb2e46262161247dd17d3f9539fed841f
aditi-s-4465 <aditi12@illinois.edu>
2025-02-21 14:33:18 -0600

    added carousel

--- posenet/src/PracticePage.js
+++ posenet/src/PracticePage.js
@@ -5,8 +5,38 @@ function PracticePage() {
   const navigate = useNavigate();
   return (
     <div>
+    
+  <div className="carousel w-full">
+  <div id="item1" className="carousel-item w-full">
+    
+  </div>
+
+  <div id="item2" className="carousel-item w-full">
+   
+  </div>
+
+  <div id="item3" className="carousel-item w-full">
+    
+  </div>
+
+  <div id="item4" className="carousel-item w-full">
+    
+  </div>
+
+</div>
+<div className="flex w-full justify-center gap-2 py-2">
+  <a href="#item1" className="btn btn-xs">1</a>
+  <a href="#item2" className="btn btn-xs">2</a>
+  <a href="#item3" className="btn btn-xs">3</a>
+  <a href="#item4" className="btn btn-xs">4</a>
+</div>
+      
+  
       <button onClick={() => navigate("/game")}> Start Game </button>
+      
     </div>
+    
+    
   );
 }
 
