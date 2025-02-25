9037dd84b5a9f3d03233823ef9f981a1a0edcddb
Chris Nee <cnee2@illinois.edu>
2025-02-24 18:54:07 -0600

    did some basic formatting

--- posenet/src/Scoreboard.js
+++ posenet/src/Scoreboard.js
@@ -5,7 +5,27 @@ function Scoreboard() {
   const navigate = useNavigate();
   return (
     <div>
-      <button onClick={() => navigate("/leaderboard")}> Go to Leaderboard </button>
+      <h1>
+        <button onClick={() => navigate("/leaderboard")}> Go to Leaderboard </button>
+      </h1>
+
+      <div style={{ height: '100vh', display: 'flex', flexDirection: 'column' }}>
+        <div style={{ backgroundColor: 'green', height: '35%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          <div style={{ backgroundColor: 'white', height: '50%', width: '60%', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', padding: '20px' }}>
+            <h1 style={{ color: 'black', fontSize: '2rem', marginBottom: '10px', textAlign: 'center' }}>
+              Score
+            </h1>
+            <p style={{ color: 'black', fontSize: '1rem', textAlign: 'center' }}>
+              Best Score
+            </p>
+          </div>
+        </div>
+      <div style={{ backgroundColor: 'blue', height: '65%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <h1 style={{ color: 'white' }}>Bottom Section </h1>
+      </div>
+    </div>
+  );
+      
     </div>
   );
 }
