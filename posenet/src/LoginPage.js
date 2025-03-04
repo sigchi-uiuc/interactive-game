2eea1089da1a31a99d8e45cb5f0a4e88a6c7d9e9
Kyubeen(Kevin) Hwang <kevinh16@illinois.edu>
2025-03-03 18:03:42 -0600

    basic button / name input implementation on LoginPage.js

--- posenet/src/LoginPage.js
+++ posenet/src/LoginPage.js
@@ -1,19 +1,43 @@
 import React from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-function LandingPage(player, setPlayer) {
+function LandingPage(player, setPlayer) {  
   /*
   Keeping with our spy analogy, const navigate is the phoneline LandingPage will use to transfer missions to other agents. useNavigate() is the company they 
   have contracted to run these services.
   */
+  const [player, setPlayer] = useState();
   const navigate = useNavigate();
+
+  // handle button click (start game) - The function name can be changed
+  const startGame = () => {
+    if (player) {
+      navigate("/practice", { state: { player } });
+    } else {
+      alert("Please enter a username to start the game.");
+    }
+  }
+
+  // need to implement return part
   return (
-    <div>
+    <div className="landing-page">
       {/*We are creating a spy gadget (component) called Button. This button sends the mission to the practice page by using the phoneline we set up earlier. Here's a 
       list of react components and the basics of how to implement them https://react.dev/reference/react-dom/components. React isn't easy, so if you find
       yourself confused trying google how to make *insert hyperspecific thing you are trying to build* in react and you'll get a bunch of helpful tutorials.
       (Sometimes mr.gpt does a real good job on telling you how to do this, but sometimes it does not)*/}
-      <button className="btn" onClick={() => navigate("/practice")}> Go to Practice </button>
+      <input
+        type="text"
+        className="input"
+        placeholder="Enter your username"
+        value={player}
+        onChange={(e) => setPlayer(e.target.value)}
+        onClick
+      />
+
+      <button className="btn" onClick={startGame}> 
+        Go to Practice
+      </button>
     </div>
   );
 }
