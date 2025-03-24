b57507330a040dccb616b7092dd52a4a45d89d61
JINGYUAN NI <nflsnjy@outlook.com>
2025-03-24 18:23:56 -0500

    change in scoreboard

--- posenet/src/Leaderboard.js
+++ posenet/src/Leaderboard.js
@@ -4,10 +4,13 @@ import { useNavigate } from "react-router-dom";
 function Leaderboard() {
   const navigate = useNavigate();
   return (
+    <>
     <div>
       <button onClick={() => navigate("/")}> Go to Login </button>
     </div>
+    <div>Div Two</div>
+    </>
   );
 }
 
-export default Leaderboard;
+export default Leaderboard;     
