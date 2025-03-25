59e704cb281a161a54c1912fa0641c28d3a407da
JINGYUAN NI <nflsnjy@outlook.com>
2025-03-24 19:00:22 -0500

    Update Scoreboard.js

--- posenet/src/Scoreboard.js
+++ posenet/src/Scoreboard.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
+const cleaverIconURL = "https://zh.wikipedia.org/wiki/%E5%BB%9A%E5%88%80#/media/File:Cucina_012.jpg"
+
 function Scoreboard(player, setPlayer, playerList, setPlayerList) {
   const navigate = useNavigate();
   return (
@@ -11,27 +13,70 @@ function Scoreboard(player, setPlayer, playerList, setPlayerList) {
 
       <div style={{ height: '100vh', display: 'flex', flexDirection: 'column' }}>
 
+        {/* left Cleaver */}
+        <img
+            src={cleaverIconURL}
+            alt="Cleaver Icon"
+            style={{
+              position: "absolute",
+              top: "50%",
+              left: 0,
+              transform: "translateY(50%)",
+              width: "50px",
+              height: "50px",
+        }} />
+            {/* Right Cleaver */}
+        <img
+              src={cleaverIconURL}
+              alt="Cleaver Icon"
+              style={{
+                position: "absolute",
+                top: "50%",
+                right: 0,
+                transform: "translateY(50%)",
+                width: "50px",
+                height: "50px",
+          }} />
+
         <div style={{ backgroundColor: 'green', height: '35%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+
           <div style={{ backgroundColor: 'white', height: '50%', width: '60%', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', padding: '20px' }}>
+
             <h1 style={{ color: 'black', fontSize: '2rem', marginBottom: '10px', textAlign: 'center' }}>
               Score
             </h1>
             <p style={{ color: 'black', fontSize: '1rem', textAlign: 'center' }}>
-              Best Score
+              Best Score: 1530
             </p>
+
           </div>
         </div>
 
         <div style={{ backgroundColor: 'blue', height: '65%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-          <h1 style={{ color: 'white' }}>Bottom Section </h1>
+          <h1 style={{ color: 'white' }}> </h1>
           
-          <div style={ {display: 'grid', gridTemplateColumns: 'repeat(1, 5fr)', gap: '10px', width: '80%', height: '80%' } } >
-            <div style={{ backgroundColor: 'lightpink', padding: '20px'}}>Person 1</div>
-            <div style={{ backgroundColor: 'lightpink', padding: '20px'}}>Person 2</div>
-            <div style={{ backgroundColor: 'lightpink', padding: '20px'}}>Person 3</div>
-            <div style={{ backgroundColor: 'lightpink', padding: '20px'}}>Person 4</div>
-            <div style={{ backgroundColor: 'lightpink', padding: '20px'}}>Person 5</div>
-            <div style={{ backgroundColor: 'lightpink', padding: '20px'}}>Person 6</div>
+          <div style={ {display: 'grid', gridTemplateColumns: 'repeat(1, 4fr)', gap: '10px', width: '80%', height: '80%' } } >
+
+            <div style={{ backgroundColor: 'lightpink', padding: '20px'}}>
+              <strong>Ranking:</strong> 10
+            </div>
+
+            <div style={{ backgroundColor: 'lightpink', padding: '20px'}}>
+              <strong>Slice Accuracy:</strong> 65%
+            </div>
+
+            <div style={{ backgroundColor: 'lightpink', padding: '20px'}}>
+              <strong>Level:</strong> 8
+            </div>
+
+            <div style={{ backgroundColor: 'lightpink', padding: '20px'}}>
+              <strong>Combo:</strong> 40
+            </div>
+
+            <div style={{ backgroundColor: 'lightpink', padding: '20px'}}>
+              <strong>Overall Time Played:</strong> 4h
+            </div>
+            
           </div>
         </div>
 
