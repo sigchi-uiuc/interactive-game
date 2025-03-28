f3ccba4bb52413c40cb2a239846e6d45af5452be
glycerm2 <glycerm2@illinois.edu>
2025-03-28 13:28:47 -0500

    added images

--- posenet/src/PracticePage.js
+++ posenet/src/PracticePage.js
@@ -2,6 +2,18 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import REACTDOM from "react-router-dom";
+
+import { ReactComponent as Grape } from "./PracticeImportFiles/Images/grape.svg";
+import { ReactComponent as HalfApple } from "./PracticeImportFiles/Images/halfapple.svg";
+import { ReactComponent as HalfPeach } from "./PracticeImportFiles/Images/halfpeach.svg";
+import { ReactComponent as Plum } from "./PracticeImportFiles/Images/plum.svg";
+import { ReactComponent as Radish } from "./PracticeImportFiles/Images/radish.svg";
+import { ReactComponent as Strawberry } from "./PracticeImportFiles/Images/strawberry.svg";
+import { ReactComponent as Banana } from "./PracticeImportFiles/Images/banana.svg";
+import { ReactComponent as Hand } from "./PracticeImportFiles/Images/hand.svg";
+import { ReactComponent as CleaverA } from "./PracticeImportFiles/Images/cleaver1.svg";
+import { ReactComponent as CleaverB } from "./PracticeImportFiles/Images/cleaver2.svg";
+import { ReactComponent as TomatoCarrot } from "./PracticeImportFiles/Images/tomatocarrot.svg";
 import './practicepage.css';
 
 function PracticePage() {
@@ -37,6 +49,8 @@ function PracticePage() {
   <div className="carousel block-c">
   
     <div id="item1" className="carousel-item div-test">
+    <Radish height= "50px" width = "50px"></Radish>
+    <HalfApple width = "50px" height= "50px"></HalfApple>
       <div className= "main-box">
         <h1>Welcome to the practice tutorial!</h1>
         <a href="#item2" onClick={() => changeIndict(1)}>
@@ -52,9 +66,13 @@ function PracticePage() {
       <div className= "main-box">
         <h1>Step One: Slicing Fruit</h1>
       </div>
+      <Hand height= "50px" width = "50px"></Hand>
+      <HalfApple width = "50px" height= "50px"></HalfApple>
       <div className= "body-para">
       <p>Slice fruit in half with</p>
       <p>vertical hand motions to gain points</p>
+      <Hand height= "50px" width = "50px"></Hand>
+      <HalfPeach width = "50px" height= "50px"></HalfPeach>
       <a href="#item3" onClick={() => changeIndict(2)}>
           <button className="login-button"> Next Step </button>
       </a>
@@ -65,12 +83,18 @@ function PracticePage() {
      <div className= "main-box">
         <h1>Step Two: When To Slice</h1>
      </div>
+     <Plum width = "50px" height= "50px" position= "fixed"></Plum>
+     <Strawberry width = "50px" height= "50px"></Strawberry>
+     <Radish width = "50px" height= "50px"></Radish>
+     <Banana width = "50px" height= "50px"></Banana>
      <div className= "body-para">
       <p>Once the fruit passing the designated cutting area, slice the fruit!</p>
       <p>**Example: Slice Vertically Here**</p>
+      <p>------------------------------------------------</p>
       <a href="#item4" onClick={() => changeIndict(3)}>
         <button className="login-button"> Next Step </button>
       </a>
+      <HalfApple width = "50px" height= "50px"></HalfApple>
      </div>
     </div>
 
@@ -78,9 +102,18 @@ function PracticePage() {
      <div className= "main-box">
         <h1>Step Three: Tips</h1>
      </div>
+     <Radish width = "50px" height= "50px"></Radish>
+     <TomatoCarrot width = "50px" height= "50px"></TomatoCarrot>
      <div className= "body-para">
-      <p>Vegetables immediately end the game</p>
-      <p>Slicing fruit combos award extra points!</p>
+     <div className= "main-box">
+        <p>Vegetables immediately end the game</p>
+      </div>
+      <div className= "main-box">
+        <p>Slicing fruit combos award extra points!</p>
+      </div>
+      <Plum width = "50px" height= "50px"></Plum>
+      <Strawberry width = "50px" height= "50px"></Strawberry>
+      <Banana width = "50px" height= "50px"></Banana>
       <a href="#item5" onClick={() => changeIndict(4)}>
         <button className="login-button"> Next Step </button>
       </a>
