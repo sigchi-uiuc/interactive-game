6ea0d97b4bad0f3ce64586c597f4f809e35b67a3
glycerm2 <glycerm2@illinois.edu>
2025-03-30 17:23:27 -0500

    in middle of positioning images

--- posenet/src/PracticePage.js
+++ posenet/src/PracticePage.js
@@ -41,24 +41,85 @@ function PracticePage() {
       }}> </button>
     }
   }
+  const correctImage = function (current) {
+    if (current[0]) {
+      return ImagesA;
+    } 
+    if (current[1]) {
+      return ImagesB;
+    }
+    if (current[2]) {
+      return ImagesC;
+    } 
+    if (current[3]) {
+      return ImagesD;
+    }
+    if (current[4]) {
+      return ImagesE;
+    }
+    return;
+  }
+  const ImagesA =
+  <div className="image-container">
+    <CleaverB className= "image tenth5-h tenth5-w"></CleaverB>
+    <HalfPeach className= "image tenth6-h tenth4-w"></HalfPeach>
+    <HalfPeach className= "image tenth7-h tenth6-w"></HalfPeach>
+  </div>
+
+  const ImagesB =
+  <div className="image-container">
+    <Hand className= "image tenth4-h tenth5-w"></Hand>
+    <HalfPeach className= "image tenth4-h tenth6-w"></HalfPeach>
+    <Hand className= "image tenth7-h tenth2-w"></Hand>
+    <HalfApple className= "image tenth7-h tenth3-w"></HalfApple>
+  </div>
+
+  const ImagesC =
+  <div className="image-container">
+    <Plum className= "image tenth1-h tenth2-w"></Plum>
+    <Strawberry className= "image tenth2-h tenth8-w"></Strawberry>
+    <Radish className= "image tenth3-h tenth8-w"></Radish>
+    <Grape className= "image tenth5-h tenth1-w"></Grape>
+    <CleaverA className= "image tenth5-h tenth7-w"></CleaverA>
+    <Banana className= "image tenth5-h tenth8-w"></Banana>
+    <HalfApple className= "image tenth7-h tenth3-w"></HalfApple>
+    <HalfApple className= "image tenth8-h tenth1-w"></HalfApple>
+  </div>
+
+  const ImagesD =
+    <div className="image-container">
+      <Radish className= "image tenth3-h tenth2-w"></Radish>
+      <TomatoCarrot className= "image tenth3-h tenth3-w"></TomatoCarrot>
+      <Plum className= "image tenth5-h tenth4-w"></Plum>
+      <Strawberry className= "image tenth5-h tenth5-w"></Strawberry>
+      <Banana className= "image tenth5-h tenth6-w"></Banana>
+    </div>
+
+  const ImagesE =
+    <div className="image-container">
+        <Grape className= "image tenth1-h tenth8-w"></Grape>
+        <Banana className= "image tenth1-h tenth6-w"></Banana>
+        <Strawberry className= "image tenth2-h tenth2-w"></Strawberry>
+        <HalfApple className= "image tenth6-h tenth2-w"></HalfApple>
+        <HalfApple className= "image tenth8-h tenth8-w"></HalfApple>
+        <Radish className= "image tenth8-h tenth1-w"></Radish>
+        <HalfPeach className= "image tenth6-h tenth8-w"></HalfPeach>
+        <Plum className= "image tenth7-h tenth4-w"></Plum>
+    </div>
 
   return (
 <div className="back">
-       <button className="home-button" onClick={() => navigate("/")}> Home </button>
-
+  <button className="home-button" onClick={() => navigate("/")}> Home </button>
+  <div position="absolute" width="100%">
+        {correctImage(currentPage)}
+  </div>
   <div className="carousel block-c">
-  
     <div id="item1" className="carousel-item div-test">
-    <Radish height= "50px" width = "50px"></Radish>
-    <HalfApple width = "50px" height= "50px"></HalfApple>
       <div className= "main-box">
         <h1>Welcome to the practice tutorial!</h1>
         <a href="#item2" onClick={() => changeIndict(1)}>
           <button className="login-button"> Begin </button>
         </a>
-      </div>
-      <div>
-
       </div>
     </div>
 
@@ -66,13 +127,9 @@ function PracticePage() {
       <div className= "main-box">
         <h1>Step One: Slicing Fruit</h1>
       </div>
-      <Hand height= "50px" width = "50px"></Hand>
-      <HalfApple width = "50px" height= "50px"></HalfApple>
       <div className= "body-para">
       <p>Slice fruit in half with</p>
       <p>vertical hand motions to gain points</p>
-      <Hand height= "50px" width = "50px"></Hand>
-      <HalfPeach width = "50px" height= "50px"></HalfPeach>
       <a href="#item3" onClick={() => changeIndict(2)}>
           <button className="login-button"> Next Step </button>
       </a>
@@ -83,10 +140,6 @@ function PracticePage() {
      <div className= "main-box">
         <h1>Step Two: When To Slice</h1>
      </div>
-     <Plum width = "50px" height= "50px" position= "fixed"></Plum>
-     <Strawberry width = "50px" height= "50px"></Strawberry>
-     <Radish width = "50px" height= "50px"></Radish>
-     <Banana width = "50px" height= "50px"></Banana>
      <div className= "body-para">
       <p>Once the fruit passing the designated cutting area, slice the fruit!</p>
       <p>**Example: Slice Vertically Here**</p>
@@ -94,7 +147,6 @@ function PracticePage() {
       <a href="#item4" onClick={() => changeIndict(3)}>
         <button className="login-button"> Next Step </button>
       </a>
-      <HalfApple width = "50px" height= "50px"></HalfApple>
      </div>
     </div>
 
@@ -102,8 +154,6 @@ function PracticePage() {
      <div className= "main-box">
         <h1>Step Three: Tips</h1>
      </div>
-     <Radish width = "50px" height= "50px"></Radish>
-     <TomatoCarrot width = "50px" height= "50px"></TomatoCarrot>
      <div className= "body-para">
      <div className= "main-box">
         <p>Vegetables immediately end the game</p>
@@ -111,9 +161,6 @@ function PracticePage() {
       <div className= "main-box">
         <p>Slicing fruit combos award extra points!</p>
       </div>
-      <Plum width = "50px" height= "50px"></Plum>
-      <Strawberry width = "50px" height= "50px"></Strawberry>
-      <Banana width = "50px" height= "50px"></Banana>
       <a href="#item5" onClick={() => changeIndict(4)}>
         <button className="login-button"> Next Step </button>
       </a>
